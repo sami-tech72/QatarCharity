9d2965a2b0d6dac4575b27d52bcc706f33dec8bb
sami-tech72 <sami.tech@floragroup.net>
2025-12-08 10:39:24 +0300

    Guard supplier bids table creation

--- src/Persistence/Migrations/20251208000000_AddSupplierBidsAndBidReviews.cs
+++ src/Persistence/Migrations/20251208000000_AddSupplierBidsAndBidReviews.cs
@@ -11,38 +11,35 @@ namespace Persistence.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "SupplierBids",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    RfxId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    SubmittedByUserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
-                    BidAmount = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
-                    Currency = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    ExpectedDeliveryDate = table.Column<DateTime>(type: "datetime2", nullable: true),
-                    ProposalSummary = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    DocumentsJson = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    InputsJson = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    SubmittedAtUtc = table.Column<DateTime>(type: "datetime2", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_SupplierBids", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_SupplierBids_AspNetUsers_SubmittedByUserId",
-                        column: x => x.SubmittedByUserId,
-                        principalTable: "AspNetUsers",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Restrict);
-                    table.ForeignKey(
-                        name: "FK_SupplierBids_Rfxes_RfxId",
-                        column: x => x.RfxId,
-                        principalTable: "Rfxes",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+            migrationBuilder.Sql(
+                """
+                IF OBJECT_ID(N'[dbo].[SupplierBids]', N'U') IS NULL
+                BEGIN
+                    CREATE TABLE [dbo].[SupplierBids](
+                        [Id] uniqueidentifier NOT NULL,
+                        [RfxId] uniqueidentifier NOT NULL,
+                        [SubmittedByUserId] nvarchar(450) NOT NULL,
+                        [BidAmount] decimal(18,2) NOT NULL,
+                        [Currency] nvarchar(max) NOT NULL,
+                        [ExpectedDeliveryDate] datetime2 NULL,
+                        [ProposalSummary] nvarchar(max) NOT NULL,
+                        [Notes] nvarchar(max) NULL,
+                        [DocumentsJson] nvarchar(max) NOT NULL,
+                        [InputsJson] nvarchar(max) NOT NULL,
+                        [SubmittedAtUtc] datetime2 NOT NULL,
+                        CONSTRAINT [PK_SupplierBids] PRIMARY KEY ([Id])
+                    );
+
+                    ALTER TABLE [dbo].[SupplierBids] ADD CONSTRAINT [FK_SupplierBids_Rfxes_RfxId]
+                        FOREIGN KEY ([RfxId]) REFERENCES [dbo].[Rfxes]([Id]) ON DELETE CASCADE;
+
+                    ALTER TABLE [dbo].[SupplierBids] ADD CONSTRAINT [FK_SupplierBids_AspNetUsers_SubmittedByUserId]
+                        FOREIGN KEY ([SubmittedByUserId]) REFERENCES [dbo].[AspNetUsers]([Id]) ON DELETE NO ACTION;
+
+                    CREATE INDEX [IX_SupplierBids_RfxId] ON [dbo].[SupplierBids] ([RfxId]);
+                    CREATE INDEX [IX_SupplierBids_SubmittedByUserId] ON [dbo].[SupplierBids] ([SubmittedByUserId]);
+                END
+                """);
 
             migrationBuilder.CreateTable(
                 name: "BidReviews",
@@ -81,16 +78,6 @@ namespace Persistence.Migrations
                 name: "IX_BidReviews_ReviewerUserId",
                 table: "BidReviews",
                 column: "ReviewerUserId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_SupplierBids_RfxId",
-                table: "SupplierBids",
-                column: "RfxId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_SupplierBids_SubmittedByUserId",
-                table: "SupplierBids",
-                column: "SubmittedByUserId");
         }
 
         /// <inheritdoc />
@@ -98,9 +85,6 @@ namespace Persistence.Migrations
         {
             migrationBuilder.DropTable(
                 name: "BidReviews");
-
-            migrationBuilder.DropTable(
-                name: "SupplierBids");
         }
     }
 }
