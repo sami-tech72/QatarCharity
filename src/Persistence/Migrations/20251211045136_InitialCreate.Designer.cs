af7ffff43844b28cd9b7a1ba2814f0bba08595e3
sami-tech72 <sami.tech@floragroup.net>
2025-12-11 08:07:59 +0300

    changes

--- src/Persistence/Migrations/20251211045136_InitialCreate.Designer.cs
+++ src/Persistence/Migrations/20251211045136_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Persistence.Context;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20251210094527_InitialCreate")]
+    [Migration("20251211045136_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
